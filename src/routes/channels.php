fef50fba79990f412ba6120b2f63b4bf58d3dd0c
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-01-12 12:07:17 +0300

    Broadcast config and channels have been initializing

--- src/routes/channels.php
+++ src/routes/channels.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+// Dynamic Presence Channel for Streaming
+Broadcast::channel('streaming-channel.{streamId}', function ($user) {
+    return ['id' => $user->id, 'name' => $user->name];
+});
+
+// Signaling Offer and Answer Channels
+Broadcast::channel('stream-signal-channel.{userId}', function ($user, $userId) {
+    return (int) $user->id === (int) $userId;
+});
