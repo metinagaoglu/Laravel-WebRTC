03d8e5d3dda86270c845c31a7492e5d40af2260c
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-01-22 21:19:35 +0300

    WebRTCStreamingController and Stream events have been created.

---
+++ src/app/Http/Controllers/WebRTCStreamingController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\StreamAnswer;
+use App\Events\StreamOffer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class WebRTCStreamingController extends Controller
+{
+    public function index() {
+        return view('video.broadcast', ['type' => 'broadcaster', 'id' => Auth::id()]);
+    }
+
+    public function consumer(Request $request, $streamId)
+    {
+        return view('video-broadcast', ['type' => 'consumer', 'streamId' => $streamId, 'id' => Auth::id()]);
+    }
+
+    public function makeStreamOffer(Request $request)
+    {
+        $data['broadcaster'] = $request->broadcaster;
+        $data['receiver'] = $request->receiver;
+        $data['offer'] = $request->offer;
+
+        event(new StreamOffer($data));
+    }
+
+    public function makeStreamAnswer(Request $request)
+    {
+        $data['broadcaster'] = $request->broadcaster;
+        $data['answer'] = $request->answer;
+        event(new StreamAnswer($data));
+    }
+}
